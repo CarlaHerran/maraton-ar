efe8448c77d75571906fdb8d39b04f23dc795b1d
codeamac20 <codeamac20@gmail.com>
2017-07-06 21:17:24 -0500

    cambios

--- db/migrate/20121011144238_create_tasks.rb
+++ db/migrate/20121011144238_create_tasks.rb
@@ -3,29 +3,36 @@ class CreateTasks < ActiveRecord::Migration[5.0]
       # Completa con las columnas que necesites
     create_table :users do |u|
       u.string :name
-      u.string :password, through: :games
+      u.string :password
     end
 
+    # UNION
     create_table :games do |g|
-      g.belongs_to :user, index: true
-      g.belongs_to :deck
+      g.belongs_to :users
+      g.belongs_to :decks
     end
 
+    # Deck que contiene preguntas
     create_table :decks do |d|
-      d.string :name
+      d.string :names
     end
 
+    # Questions pertenecen a deck, y tiene un "nombre" cada pregunta. Cada pregunta corresponde a un answer.
     create_table :questions do |q|
-      q.string :deck
+      q.belongs_to :decks
+      q.string :name
     end
+      # Answers pertenecen a questions.
     create_table :answers do |a|
-      a.string :question
+      a.belongs_to :questions
+      a.string :name
     end
+    # Es quien contiene las respuestas correctas e incorrectas a cada pregunta correspondiente de cada game.
 
     create_table :results  do |r|
-      r.belongs_to :game
-      r.belongs_to :question
-      r.belongs_to :answer
+      r.belongs_to :games
+      r.belongs_to :questions
+      r.belongs_to :answers
     end
 
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # Este archivo sirve para crear registros de prueba
-Question.create(deck: 'Award winning Latina pop artist Shakira was born in raised in what Country?', answer: 'Colombia')
-Question.create(deck: 'Which British author wrote the popular childrens novel James and the Giant Peach?', answer: 'Roald Dahl')
-Question.create(deck: 'In the Lord of the Rings film series which actor plays the character of Saruman?', answer: 'Christopher Lee')
-Question.create(deck: 'In the Star Wars universe, who is Luke Skywalkers mother?', answer: 'Padmé Amidala')
-Question.create(deck:'The inventor Nikola Tesla was born on July 10th 1856 in what country?', answer: 'Serbia')
+Question.create(name: 'Award winning Latina pop artist Shakira was born in raised in what Country?')
+Question.create(name: 'Which British author wrote the popular childrens novel James and the Giant Peach?')
+Question.create(name: 'In the Lord of the Rings film series which actor plays the character of Saruman?')
+Question.create(name: 'In the Star Wars universe, who is Luke Skywalkers mother?')
+Question.create(name:'The inventor Nikola Tesla was born on July 10th 1856 in what country?')
